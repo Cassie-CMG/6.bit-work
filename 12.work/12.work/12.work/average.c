5f8e7d416cd1ac7c71d754dd92181ba816bf1388
Cassie-CMG <CMG_Cassie@163.com>
2018-11-04 16:18:54 +0800

    位运算实现

--- 12.work/12.work/12.work/average.c
+++ 12.work/12.work/12.work/average.c
@@ -1,27 +1,31 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int Average(int x,int y)
-//{
-//	int average = 0;
-//	if (x > y)
-//	{
-//		average = y + (x - y) / 2;
-//	}
-//	else
-//	{
-//		average = x + (y - x) / 2;
-//	}
-//	return average;
-//}
-//
-//int main()
-//{
-//	int num1 = 10, num2 = 20;
-//	int average = 0;
-//	average = Average(num1, num2);
-//	printf("平均数为%d\n", average);
-//	system("pause");
-//	return 0;
-//}
+//求两个数的平均数
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int average1(int m, int n)
+{
+	return (m + n) >> 1;
+}
+
+int average2(int m, int n)
+{
+	return (m & n) + ((m ^ n) >> 1);
+}
+
+int average3(int m, int n)
+{
+	return m + ((n - m) >> 1);
+}
+
+int main()
+{
+	int m = 0, n = 0;
+	printf("请输入两个数：");
+	scanf("%d %d", &m, &n);
+	printf("平均数为：%d\n", average1(m, n));
+	printf("平均数为：%d\n", average2(m, n));
+	printf("平均数为：%d\n", average3(m, n));
+	system("pause");
+	return 0;
+}
