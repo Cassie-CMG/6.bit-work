4388cb2d73ba7f0528f4088a2a3736bfaaaa8ba1
Cassie-CMG <CMG_Cassie@163.com>
2018-11-05 15:39:47 +0800

    注释更改

--- 6.work/6.work/5.c
+++ 6.work/6.work/5.c
@@ -1,31 +1,31 @@
-////求一个字符串的长度
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int strlen1(char* str)
-//{
-//	int count = 0;
-//	while(*str != '\0')
-//	{
-//		count++;
-//		str++;
-//	}
-//	return count;
-//}
-//
-//int strlen2(char* str)
-//{
-//	if (*str == '\0')
-//	{
-//		return 0;
-//	}
-//	return 1 + strlen2(str + 1);
-//}
-//
-//int main()
-//{
-//	printf("%d\n", strlen1("hello"));
-//	printf("%d\n", strlen2("hello"));
-//	system("pause");
-//	return 0;
-//}
+//求一个字符串的长度
+#include <stdio.h>
+#include <stdlib.h>
+
+int strlen1(char* str)
+{
+	int count = 0;
+	while(*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+int strlen2(char* str)
+{
+	if (*str == '\0')
+	{
+		return 0;
+	}
+	return 1 + strlen2(str + 1);
+}
+
+int main()
+{
+	printf("%d\n", strlen1("hello"));
+	printf("%d\n", strlen2("hello"));
+	system("pause");
+	return 0;
+}
--- 6.work/6.work/6.c
+++ 6.work/6.work/6.c
@@ -1,19 +1,19 @@
-////求一个非负整数的各数位之和
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//int DigitSum(int n)
-//{
-//	if (n > 9)
-//	{
-//		return n % 10 + DigitSum(n / 10);
-//	}
-//	return n;
-//}
-//
-//int main()
-//{
-//	printf("%d\n", DigitSum(1729));
-//	system("pause");
-//	return 0;
-//}
+//求一个非负整数的各数位之和
+#include <stdio.h>
+#include <stdlib.h>
+
+int DigitSum(int n)
+{
+	if (n > 9)
+	{
+		return n % 10 + DigitSum(n / 10);
+	}
+	return n;
+}
+
+int main()
+{
+	printf("%d\n", DigitSum(1729));
+	system("pause");
+	return 0;
+}
